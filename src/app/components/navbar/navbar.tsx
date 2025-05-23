f6d782761f75b6e70bb37d174ccc763da32f7cb0
shariqshanu <shariqshanu0@gmail.com>
2025-05-23 23:34:57 +0500

    landing project link update

---
+++ src/app/components/navbar/navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+import Image from "next/image";
+import Link from "next/link";
+import HeroSection from "@/app/components/heropage";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navbarRef = useRef<HTMLDivElement>(null); // Specify the type explicitly
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navbarRef.current && !navbarRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+      toggleMenu(); // Close menu after scrolling
+    }
+  };
+
+  const handleClick = (sectionId: string) => (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault(); // Prevent default Link behavior
+    scrollToSection(sectionId);
+  };
+
+
+  return (
+    <div className="header-section" id="header">
+      <div className="container mx-auto px-2" ref={navbarRef}>
+        <div className="navbar">
+          <nav className="pt-2">
+            <Link
+              href="/"
+              className=" flex text-[#16f2b3] text-3xl font-bold gap-2"
+              style={{ fontSize: "1.9rem", cursor: "pointer" }}
+            >
+              {/*<Image*/}
+              {/*  src="/images/myimages/shariq_logo.png"*/}
+              {/*  width={200}*/}
+              {/*  height={200}*/}
+              {/*  alt="not shown"*/}
+              {/*  className={"p-2"}*/}
+              {/*/>*/}
+              {/*<h2>Shariq</h2>*/}
+            </Link>
+
+            <ul className={menuOpen ? "active" : ""}>
+
+            
+
+              <li>
+                <Link href="#about"   onClick={handleClick("about")}>About</Link>
+              </li>
+              <li>
+                <Link href="#skills" onClick={handleClick("skills")}>Skills</Link>
+              </li>
+              <li>
+                <Link href="#experience"onClick={handleClick("experience")}>Experience</Link>
+              </li>
+              <li>
+                <Link href="#projects" onClick={handleClick("projects")}>Projects</Link>
+              </li>
+              <li>
+                <Link href="#education" onClick={handleClick("education")}>Education</Link>
+              </li>
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className="text-white"
+                onClick={toggleMenu}
+              />
+            </ul>
+            <FontAwesomeIcon
+              icon={faBars}
+              onClick={toggleMenu}
+              className="text-white"
+            />
+          </nav>
+        </div>
+        <HeroSection />
+      </div>{" "}
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/app/data/projects-data.ts
@@ -0,0 +1,38 @@
+import prayerwar from "../images/projects/prayerwar.jpeg"
+import blackraters from "../images/projects/blackraters.jpeg"
+import tailwindCSS from "../images/projects/tailwind-css.jpeg"
+
+const DataCard = [
+    {
+        id: "0",
+        image: prayerwar,
+        heading: "Prayer War",
+        detail:
+            "PrayerWar is a platform where Christians can share their prayer requests and comment on their acceptance. It is a supportive Christian community that offers many other features through faith and prayer.",
+        demo: "https://prayerwars.customwebsitedemo.host/",
+        // github: "",
+        techused:['Laravel']
+    },
+    {
+        id: "1",
+        image: blackraters,
+        heading: "Black Raters",
+        detail:
+            "Black Raters is a site for movie lovers, where you can rate, review, and talk about films. It’s a great place to watch movies and share what you think about them.",
+        demo: "https://blackraters.com/",
+        // github: "",
+        techused: ['Laravel']
+    },
+    {
+        id: "2",
+        image: tailwindCSS,
+        heading: "Landing Page",
+        detail:
+            "This landing page is created using Tailwind CSS as a practice project. It showcases responsive design and modern UI components, demonstrating the power and flexibility of Tailwind for building beautiful web interfaces.",
+        demo: "https://tailwind-landing-page-github-io.vercel.app/",
+        // github: "",
+        techused: ['Tailwind CSS']
+    }
+];
+
+export default DataCard;
